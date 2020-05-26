fe7b9db8aafdf5a65fd7b1b2806c1281d05b7ae6
cesar willy mamani canaza <cesarwilly.mc@gmail.com>
2020-05-25 23:16:15 -0500

    por fin capta alaoz pueblo

--- app/src/main/java/com/consorciosm/sanmiguel/ui/main/supervisor/SupervisorActivity.kt
+++ app/src/main/java/com/consorciosm/sanmiguel/ui/main/supervisor/SupervisorActivity.kt
@@ -8,10 +8,7 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.onNavDestinationSelected
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
 import com.consorciosm.sanmiguel.R
 import com.consorciosm.sanmiguel.base.BaseActivity
 import com.consorciosm.sanmiguel.ui.main.MainViewModelFactory
@@ -35,66 +32,25 @@ class SupervisorActivity : BaseActivity(),KodeinAware, AppBarConfiguration.OnNav
         viewModel= run {
             ViewModelProvider(this,factory).get(ViewModelMain::class.java)
         }
-
-
-
         setSupportActionBar(toolbar)
-        val headerView = navview.getHeaderView(0)
-        setInfoUser(headerView)
+//        val headerView = navview.getHeaderView(0)
+//        setInfoUser(headerView)
         navController = findNavController(R.id.nav_host_supervisor)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawer_layout)
+            R.id.nav_registro, R.id.nav_registro_vehiculo, R.id.nav_partes
+        ) , drawer_layout)
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        //, R.id.nav_permisos,
-        //                R.id.nav_personal,R.id.nav_validacion,R.id.nav_perfil
-        setupNavigation(navController) //setup navigation
-        setupActionBar(navController, appBarConfiguration)
+        navview.setupWithNavController(navController)
 
     }
-    private fun setupActionBar(
-        navController: NavController,
-        appBarConfig: AppBarConfiguration
-    ) {
-        setupActionBarWithNavController(navController, appBarConfig)
-    }
-    private fun setupNavigation(navController: NavController) {
-        val sideNavView = findViewById<NavigationView>(R.id.nav_view)
-        sideNavView?.setupWithNavController(navController)
-        val drawerLayout: DrawerLayout? = findViewById(R.id.drawer_layout)
 
-        //fragments load from here but how ?
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_registro, R.id.nav_registro_vehiculo, R.id.nav_partes
-            ) , drawerLayout
-        )
-    }
+
     private fun setInfoUser(headerView: View?) {
 
     }
 
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        //I need to open the drawer onClick
-        when (item!!.itemId) {
-            android.R.id.home ->
-                drawer_layout.openDrawer(GravityCompat.START)
-        }
-        return item.onNavDestinationSelected(findNavController(R.id.nav_host_fragment))
-                || super.onOptionsItemSelected(item)
-    }
-    override fun onBackPressed() {
-        //the code is beautiful enough without comments
-        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
-            drawer_layout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 //    override fun onSupportNavigateUp(): Boolean {
 //        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
